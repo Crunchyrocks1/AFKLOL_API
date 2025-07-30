9ab7ba048d35e522c0c5b7ee81d57643fd8bfea1
Scooby.Dev <150474806+Crunchyrocks1@users.noreply.github.com>
2025-07-30 18:31:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-NAME = "NEXUS"
+brands = {
+    "NEXUS": {
+        "NAME": "NEXUS",
+        "DISCORD_URL": "https://discord.gg/Aq8uTZqVZN",
+        "COLOR": "blue"
+    },
+    "ECORE": {
+        "NAME": "ECORE",
+        "DISCORD_URL": "https://discord.gg/ecore",
+        "COLOR": "green"
+    },
+}
 
-@app.get("/variables")
-def get_variables():
-    return {"NAME": NAME}
+@app.get("/variables/{brand_name}")
+def get_variables(brand_name: str):
+    brand_data = brands.get(brand_name.upper())
+    if not brand_data:
+        raise HTTPException(status_code=404, detail="Brand not found")
+    return brand_data
